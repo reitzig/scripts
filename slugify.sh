36249c003fd5860b147f9e34b1bd6768b458cd0d
Raphael Reitzig <4246780+reitzig@users.noreply.github.com>
2025-09-11 09:08:22 +0200

    fix(slugify): handle umlauts; lower-case

--- slugify.sh
+++ slugify.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-[ ${#} -ge 1 ] && input="${1}" || read -r input
+set -euo pipefail
 
-echo "${input}" | sed 's/[^[:alnum:]\.\_]\+/-/g'
+{ [ ${#} -ge 1 ] && input="${1}"; } \
+    || read -r input
+
+# NB:
+#  - set locale to C so that umlauts aren't matched by `a-z`;
+#     see also https://unix.stackexchange.com/q/390454
+#  - first sed script replaces illegal characters
+#  - second one lower-cases the whole thing
+# shellcheck disable=SC2001
+echo "${input}" | LC_ALL=C sed -e 's/[^a-zA-Z0-9\.\_]\+/-/g' -e 's/\(.*\)/\L\1/'
 
