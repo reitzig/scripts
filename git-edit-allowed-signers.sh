cddfce569a5020a165964e95be9746e96e150d58
Raphael Reitzig <4246780+reitzig@users.noreply.github.com>
2026-08-25 18:50:55 +0200

    fix(git-edit-allowed-signers): abort early if not in a git repo

--- git-edit-allowed-signers.sh
+++ git-edit-allowed-signers.sh
@@ -5,6 +5,10 @@ set -euo pipefail
 allowed_signers_file="$(pwd)/.git/info/allowed_signers"
 editor="${EDITOR:-vi}"
 
+git status > /dev/null # aborts if current dir isn't a Git repo
+
+# TODO: if a different file is already configured, ask whether to overwrite or use that one
+
 if [[ ! -e "${allowed_signers_file}" ]]; then
   cat <<TXT > "${allowed_signers_file}"
 # List of known email-key pairs
@@ -13,6 +17,8 @@ if [[ ! -e "${allowed_signers_file}" ]]; then
 # Example:
 #   some.person@some.org ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAI109FF5EFDFF74260919CB8F54A5E7CF2
 TXT
+
+  # TODO: pre-populate with global setting, if any?
 fi
 
 git config 'gpg.ssh.allowedSignersFile' "${allowed_signers_file}"
